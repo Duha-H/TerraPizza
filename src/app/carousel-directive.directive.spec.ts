5ef1f7e69d0826e82b6118331f465e5de7ac44c8
Duha-H <duha.h.153@gmail.com>
2020-05-10 00:47:39 -0400

    Auto-generated commit

--- src/app/carousel-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CarouselDirectiveDirective } from './carousel-directive.directive';
-
-describe('CarouselDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CarouselDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- temp.md
+++
@@ -1,2 +0,0 @@
-client-side prediction/generation
-pretrained model
